90f32f8af305d41ba453ab8c9108c462d2cb74ea
品川 広樹 <88636@ad.exa-corp.co.jp>
2020-12-05 21:26:10 +0900

    サイドバーを修正

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+
+
 import './Sidebar.css';
 
 function Sidebar() {
+
+    const [checked, setChecked] = React.useState([0]);
+
+    const handleToggle = (index) => () => {
+        const currentIndex = checked.indexOf(index);
+        const newChecked = [...checked];
+
+        if (currentIndex === -1) {
+            newChecked.push(index);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
+
+        setChecked(newChecked);
+    };
+
     return (
         <div className="sidebar">
-            <span>Sidebar</span>
+            <List>
+                {["React", "Java", "人工知能", "AWS", "React", "Java", "人工知能", "AWS"].map((value, index) => {
+                    const labelId = `checkbox-list-label-${value}`;
+
+                    return (
+                        <ListItem key={value} role={undefined} dense button onClick={handleToggle(index)}>
+                            <ListItemIcon>
+                                <Checkbox
+                                    edge="start"
+                                    checked={checked.indexOf(index) !== -1}
+                                    tabIndex={-1}
+                                    disableRipple
+                                    inputProps={{ 'aria-labelledby': labelId }}
+                                />
+                            </ListItemIcon>
+                            <ListItemText id={labelId} primary={value} />
+                        </ListItem>
+                    );
+                })}
+            </List>
         </div>
     );
 }
