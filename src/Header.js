0c8a9f3cf1f0b263e560e33a51f81a364024db67
unknown <yojinakano@gmail.om>
2020-12-05 15:39:32 +0900

    arrenge header item

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,50 @@
 import React from 'react';
 import './Header.css';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuIcon from '@material-ui/icons/Menu';
+import Grid from '@material-ui/core/Grid'
 
 function Header() {
-    return(
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
         <div className="header">
-            <span>Header</span>
+            <Grid item xs={10}>
+                <h1>Zatta</h1>
+            </Grid>
+
+            <Grid item xs={1}>
+                <span id='user'>user1</span>
+            </Grid>
+
+            <Grid item xs={1}>
+
+                <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                    <MenuIcon id='menu' fontSize="large" />
+                </Button>
+                <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >
+                    <MenuItem onClick={handleClose}>Home</MenuItem>
+                    <MenuItem onClick={handleClose}>検索設定(Fess)</MenuItem>
+                    <MenuItem onClick={handleClose}>キーワード登録</MenuItem>
+                    <MenuItem onClick={handleClose}>ログアウト</MenuItem>
+                </Menu>
+            </Grid>
         </div>
     );
 }
